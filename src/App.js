d12621f62b1cc660f2d5426941509c6bbab36499
Adam Houston <phazyy9@gmail.com>
2016-03-31 14:15:45 +0100

    Adding material-ui, basic app bar and scss loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+import LeftNav from 'material-ui/lib/left-nav';
+import MenuItem from 'material-ui/lib/menus/menu-item';
 
-export default React.createClass({
-  render() {
-    return <div>
-      <h2>Welcome to React</h2>
-    </div>
+//import scss from 'static/styles/base.scss'; Add scss loader
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+
+    // Components using ES6 classes don't autobind 'this' to non React methods
+    this.handleToggle = this.handleToggle.bind(this)
   }
-})
+
+  handleToggle() {
+    this.setState({ open: !this.state.open })
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="Futie"
+          onLeftIconButtonTouchTap={this.handleToggle}
+          iconClassNameRight='muidocs-icon-navigation-expand-more'
+        />
+      <LeftNav open={this.state.open}>
+          <MenuItem onTouchTap={this.handleToggle}>Leagues</MenuItem>
+          <MenuItem onTouchTap={this.handleToggle}>Players</MenuItem>
+          <MenuItem onTouchTap={this.handleToggle}>Settings</MenuItem>
+        </LeftNav>
+      </div>
+    )
+  }
+}
+
+export  default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import {render} from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import App from './App'
+import scss from './static/styles/base.scss'
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 render(<App/>, document.querySelector('#app'))
