9805ff46e82d306b5f190c770fcc43567687d7c9
Adam Houston <phazyy9@gmail.com>
2016-04-06 16:25:56 +0100

    Adding match score/time formatting

--- src/components/ScoreList.js
+++ src/components/ScoreList.js
@@ -4,16 +4,32 @@ import { connect } from 'react-redux'
 class ScoreList extends React.Component {
   constructor(props) {
     super(props);
+    this.handleMatchStatus = this.handleMatchStatus.bind(this);
+  }
+
+  handleMatchStatus(score) {
+    let score_time = '00:00'
+    if (score.status == 'FINISHED') {
+      score_time = [
+        score.result.goalsHomeTeam,
+        ' - ',
+        score.result.goalsAwayTeam
+      ].join('');
+    }
+    else {
+      // 11 Time start index, 16 Time end index. Make this dynamic later
+      score_time - score.date.substring(11, 16);
+    }
+    return score_time;
   }
 
   render() {
     const { scores } = this.props;
-
     return (
       <ul>
         { scores.map(score =>
           <li>
-            <span>{ score.homeTeamName } v { score.awayTeamName }</span>
+            <span>{ score.homeTeamName } { this.handleMatchStatus(score) } { score.awayTeamName }</span>
           </li>
         )}
       </ul>
