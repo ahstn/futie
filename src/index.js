e2bbd17bf3651c448d2b8dfbe4fd54b507ffe8e1
Adam Houston <phazyy9@gmail.com>
2016-04-14 10:40:38 +0100

    Separating the store to it's own file and adding logger middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './containers/App';
+import configureStore from './store/configureStore'
 import rootReducer from './reducers/root';
 import scss from './static/styles/base.scss';
 
@@ -15,10 +16,7 @@ import scss from './static/styles/base.scss';
 //https://github.com/zilverline/react-tap-event-plugin
 injectTapEventPlugin();
 
-let store = createStore(
-  rootReducer,
-  applyMiddleware(thunkMiddleware)
-);
+const store = configureStore()
 
 render(
   <Provider store={store}>
---
+++ src/store/configureStore.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import rootReducer from '../reducers/root';
+
+export default function configureStore(initialState) {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunkMiddleware, createLogger())
+  )
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers/root', () => {
+      const nextRootReducer = require('../reducers/root').default
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+
+  return store
+}
