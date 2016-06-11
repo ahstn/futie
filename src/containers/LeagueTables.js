47428522d7a4c66b617443f3385db8d6e7d5901f
Adam <phazyy9@gmail.com>
2016-06-11 10:05:57 +0100

    Adding League Tables

--- src/containers/LeagueTables.js
+++ src/containers/LeagueTables.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Card from 'material-ui/lib/card/card';
+import CardActions from 'material-ui/lib/card/card-actions';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardText from 'material-ui/lib/card/card-text';
+import Table from 'material-ui/lib/table/table';
+import TableHeaderColumn from 'material-ui/lib/table/table-header-column';
+import TableRow from 'material-ui/lib/table/table-row';
+import TableHeader from 'material-ui/lib/table/table-header';
+import TableRowColumn from 'material-ui/lib/table/table-row-column';
+import TableBody from 'material-ui/lib/table/table-body';
 
 import { fetchLeagueTable } from '../actions/leagues';
 
@@ -18,9 +28,35 @@ class LeagueTables extends Component {
   render() {
     const { leagues } = this.props;
     return (
-      <div>
-        <h2>Hi { leagues.leagueCaption }</h2>
-      </div>
+      <Card>
+        <CardHeader title={ leagues.leagueCaption } />
+        <CardText>
+          <Table selectable={ false }>
+            <TableHeader
+              adjustForCheckbox={ false }
+              displaySelectAll={ false }>
+              <TableRow>
+                <TableHeaderColumn>#</TableHeaderColumn>
+                <TableHeaderColumn>Team</TableHeaderColumn>
+                <TableHeaderColumn>PG</TableHeaderColumn>
+                <TableHeaderColumn>GD</TableHeaderColumn>
+                <TableHeaderColumn>Pts</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={ false }>
+              { leagues.standing.map((team) =>
+                <TableRow>
+                  <TableRowColumn>{ team.position }</TableRowColumn>
+                  <TableRowColumn>{ team.teamName }</TableRowColumn>
+                  <TableRowColumn>{ team.playedGames }</TableRowColumn>
+                  <TableRowColumn>{ team.goalDifference }</TableRowColumn>
+                  <TableRowColumn>{ team.points }</TableRowColumn>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </CardText>
+      </Card>
     );
   }
 }
